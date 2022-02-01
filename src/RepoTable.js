fdaf8defecf45cf76b9dad10df13582c4b5cafbe
Isabel Su <isabelsu@Isabels-MacBook-Pro.local>
2022-02-01 01:59:27 -0500

    fix format

--- src/RepoTable.js
+++ src/RepoTable.js
@@ -66,7 +66,6 @@ export default function RepoTable({ items }) {
                   >
                     Details
                   </Button>
-
                 </td>
               </tr>
             );
